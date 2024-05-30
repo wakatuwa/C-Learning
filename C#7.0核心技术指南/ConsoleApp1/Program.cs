111afc26745b27cf27856b17ebe1aab8890545e9
han wang <wanghanmaster@163.com>
2024-05-30 22:41:28 +0800

    C#7.0核心技术指南first

---
+++ C#7.0核心技术指南/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FeetToInches(30));
+            Console.WriteLine(FeetToInches(100));
+        }
+        static int FeetToInches(int feet)
+        {
+            int inches = feet * 12;
+            return inches;
+        }
+    }
+}
